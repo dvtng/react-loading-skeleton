33e1e8ad39066400527ffa0d4af6dcd63fefc77c
Sander Vispoel <contact@sandervispoel.com>
2019-04-30 15:04:45 +0200

    Add types for SkeletonTheme

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,19 @@
 import { Component, ReactNode } from "react";
 
-export default class Skeleton extends Component<Props> {}
+export default class Skeleton extends Component<SkeletonProps> {}
 
-interface Props {
+interface SkeletonProps {
   count?: number;
   duration?: number;
   width?: string;
   wrapper?: ReactNode;
   height?: string;
   circle?: boolean;
+}
+
+export class SkeletonTheme extends Component<SkeletonThemeProps> {}
+
+interface SkeletonThemeProps {
+  color: string;
+  highlightColor: string;
 }
