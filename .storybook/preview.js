416ab4b3b2138d2d53453c9ab67943173db8919d
Sam Magura <srmagura@gmail.com>
2021-10-08 13:59:21 -0400

    Prettify storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
 }
