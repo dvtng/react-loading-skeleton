d8d35888281fe75d9b3ea02b00822dccc55ede41
Rosie Magura <Rosie.Magura@iticentral.com>
2021-10-11 12:34:23 -0400

    MSC-5 Edits

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -1,12 +1,9 @@
 import typescript from '@rollup/plugin-typescript'
 import copy from 'rollup-plugin-copy'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import React from 'react'
-import replace from '@rollup/plugin-replace'
 
 export default {
     input: 'src/index.ts',
+    external: ['react'],
     output: [
         {
             file: 'dist/index.mjs',
@@ -37,19 +34,9 @@ export default {
         },
     ],
     plugins: [
-        replace({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-            preventAssignment: true,
-        }),
         typescript({ exclude: ['**/__tests__/**/*', '**/__stories__/**/*'] }),
         copy({
             targets: [{ src: 'src/skeleton.css', dest: 'dist' }],
         }),
-        commonjs({
-            namedExports: {
-                react: Object.keys(React),
-            },
-        }),
-        resolve(),
     ],
 }
